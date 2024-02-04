450523ee323fe05fcff399fa50474d3ef95dbd84
Max <max@maximilian.co.nz>
2024-02-04 21:40:07 +1300

    Stole movement code from fps (edited a bit)

--- src/Editor/Viewer.cs
+++ src/Editor/Viewer.cs
@@ -4,6 +4,17 @@ using Raylib_cs;
 class Viewer
 {
 	public static Camera3D Camera;
+	public static Vector3 Position { get; set; } = new Vector3(0, 0, 0);
+	public static float Pitch { get; set; } = 0;
+	public static float Yaw { get; set; } = 0;
+
+	private static float sensitivity = 150f;
+	private static float speed = 70f;
+
+	private static Vector3 forward;
+	private static Vector3 forwardDirection;
+	private static Vector3 right;
+	private static Vector3 up;
 
 	public static void Start()
 	{
@@ -22,6 +33,67 @@ class Viewer
 
 	public static void Update()
 	{
-		Raylib.UpdateCamera(ref Camera, CameraMode.CAMERA_FREE);
+		MouseMovement();
+		KeyboardMovement();
+
+		// Update the camera
+		Camera.position = Position;
+		Camera.target = Camera.position + forwardDirection;
+	}
+
+	private static void MouseMovement()
+	{
+		// Get the mouse movement in degrees
+		Vector2 mouseDelta = Raylib.GetMouseDelta() * sensitivity;
+		float mouseX = mouseDelta.X / Raylib.GetScreenWidth();
+		float mouseY = mouseDelta.Y / Raylib.GetScreenHeight();
+
+		// Update the pitch and yaw values
+		Pitch -= mouseY;
+		Yaw -= mouseX;
+
+		// Clamp the pitch from -90 to 90 in degrees
+		// and keep yaw between 0 and 360 degrees
+		// TODO: Remove gimbal lock
+		Pitch = Math.Clamp(Pitch, -90f, 90f);
+		Yaw %= 360f;
+
+		// Convert into a quaternion to make working with rotations easier
+		Quaternion rotation = Quaternion.CreateFromYawPitchRoll(DegreesToRadians(Yaw), DegreesToRadians(Pitch), 0);
+
+		// Calculate target for moving forwards and right
+		forward = Vector3.Transform(new Vector3(0, 0, -1), rotation);
+		right = Vector3.Transform(new Vector3(1, 0, 0), rotation);
+
+		// Assign the direction Forward for controlling camera rotation
+		forwardDirection = Vector3.Transform(new Vector3(0, 0, -1), rotation);
+	}
+
+
+	private static void KeyboardMovement()
+	{
+		float dt = Raylib.GetFrameTime();  // Delta time - time since last frame
+
+		// Move forward
+		if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+			Position += forward * speed * dt;
+
+		// Move backward
+		if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+			Position -= forward * speed * dt;
+
+		// Strafe right
+		if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+			Position += right * speed * dt;
+
+		// Strafe left
+		if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+			Position -= right * speed * dt;
+	}
+
+
+	private static float DegreesToRadians(float degrees)
+	{
+		return degrees * (MathF.PI / 180);
 	}
 }
